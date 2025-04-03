ff20b490f43ad25813e48a5abea0b9805d43259e
Sunil Dutt <sunil.dutt28@gmail.com>
2025-04-03 23:10:25 +0100

    one line

--- hello.py
+++ hello.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")#URL leading to method
 def hello(): # Name of the method
- return("Hello from Flask App!") #indent this line
+ return("Hello from Flask App! file stored in git") #indent this line
 if __name__ == "__main__":
  app.run(host='0.0.0.0', port='8080') 
