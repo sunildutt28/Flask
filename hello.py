e1371ef7b2add2abae5443363b554470182e60fa
sunildutt28 <125875046+sunildutt28@users.noreply.github.com>
2025-04-08 21:39:39 +0100

    Update hello.py
    few

--- hello.py
+++ hello.py
@@ -2,6 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")#URL leading to method
 def hello(): # Name of the method
- return("Hello from Flask App! file stored in git") #indent this line
+ return("Hello !
+        Sunil here ...
+        from Flask App! 
+        and file stored in git") #indent this line
 if __name__ == "__main__":
  app.run(host='0.0.0.0', port='8080') 
