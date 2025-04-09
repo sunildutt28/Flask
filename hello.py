756c81c58b661f822e231e2a27231133e95efe2e
Sunil Dutt <sunil.dutt28@gmail.com>
2025-04-09 18:27:03 +0100

    https

--- hello.py
+++ hello.py
@@ -4,4 +4,5 @@ app = Flask(__name__)
 def hello(): # Name of the method
  return("Hello ! \n Sunil here ... \n from Flask App! \n and python file stored in git") #indent this line
 if __name__ == "__main__":
- app.run(host='0.0.0.0', port='8080') 
+ #app.run(host='0.0.0.0', port='8080') 
+    app.run(ssl_context=("cert.pem", "key.pem"), debug=True)
